a423756523a7536d2deb0777eb6a13b56ce3844e
xFatal1tYx <62523739+xFatal1tYx@users.noreply.github.com>
2022-12-20 03:31:24 +0300

    Create 3.java

---
+++ 2 Примитивные типы/2.2 Преобразование типов/3.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
+        byte c = a + b; //Incompatible types. Found: 'int', required: 'byte'
+    }
+
+}
