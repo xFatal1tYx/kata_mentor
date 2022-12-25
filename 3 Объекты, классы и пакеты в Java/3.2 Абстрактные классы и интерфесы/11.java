c35996dc19c9ed253693e0d6c193dcaa806e3f78
xFatal1tYx <62523739+xFatal1tYx@users.noreply.github.com>
2022-12-25 22:39:34 +0300

    Create 11.java

---
+++ 3 Объекты, классы и пакеты в Java/3.2 Абстрактные классы и интерфесы/11.java
@@ -0,0 +1,18 @@
+// абстрактный класс, без абстрактных методов
+abstract class Animal {
+    private final String name;
+    private final String color;
+
+    // конструктор, для инициализации полей, при создании наследника
+    Animal(String name, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+}
+
+class Cat extends Animal {
+    Cat(String name, String color) {
+        super(weight, color);
+    }
+}
