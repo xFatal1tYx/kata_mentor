079b0e59ee238a709c07ad672e0da592af5ef8f4
xFatal1tYx <62523739+xFatal1tYx@users.noreply.github.com>
2022-12-25 21:52:36 +0300

    Create 3.java

---
+++ 3 Объекты, классы и пакеты в Java/3.2 Абстрактные классы и интерфесы/3.java
@@ -0,0 +1,44 @@
+enum Wishes {
+    RESPECTFULLY, SINCERELY, GRATITUDE, WISHES
+}
+
+interface Printable {
+
+    //final public static поле
+    String signature = "@Author";
+
+    //abstract public метод
+    void print();
+
+    //static метод
+    static String getWishes(Wishes wish) {
+        return switch (wish) {
+            case RESPECTFULLY -> "\n\nRespectfully yours ";
+            case SINCERELY -> "\n\nSincerely yours ";
+            case GRATITUDE -> "\n\nWith gratitude ";
+            default -> "\n\nBest wishes ";
+        };
+    }
+
+    //default public метод с реализацией
+    default String printCaption() {
+        return "=== Caption ===\n\n";
+    }
+
+}
+
+class DefaultText implements Printable {
+    @Override
+    public void print() {
+        System.out.print(printCaption());
+        System.out.print("Body text");
+        System.out.print(Printable.getWishes(Wishes.SINCERELY) + Printable.signature);
+    }
+}
+
+public class Main  {
+    public static void main(String[] args) {
+        DefaultText text = new DefaultText();
+        text.print();
+    }
+}
