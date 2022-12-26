6a9ca011d6496781893ca4cc91766069ce935f0b
xFatal1tYx <62523739+xFatal1tYx@users.noreply.github.com>
2022-12-26 04:21:36 +0300

    Create 15.java

---
+++ 3 Объекты, классы и пакеты в Java/3.3 Модификаторы доступа, ключевые слова final и static/15.java
@@ -0,0 +1,10 @@
+// Верхние классы не могут быть static, потому что static члены должны сами по себе принадлежать какому-то классу.
+
+// Modifier 'static' not allowed here
+public static class Main  {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
