a57c8a12d877d288fdc003572beae154e69da325
xFatal1tYx <62523739+xFatal1tYx@users.noreply.github.com>
2023-01-03 04:00:25 +0300

    Create 13.java

---
+++ 4 Обработка ошибок, исключения, отладка/4.2 Обработка исключений. Try-catch/13.java
@@ -0,0 +1,12 @@
+// Второе исключение подавит первое.
+// Вывод: Exception in thread "main" java.lang.RuntimeException: something went wrong x2
+
+public class Main {
+    public static void main(String... args)  {
+        try {
+            throw new RuntimeException("something went wrong");
+        } finally {
+            throw new RuntimeException("something went wrong x2");
+        }
+    }
+}
