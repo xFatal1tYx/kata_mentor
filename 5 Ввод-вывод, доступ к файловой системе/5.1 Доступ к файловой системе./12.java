a8f479ea914ee7c33882d6c678a956da9733fb3d
xFatal1tYx <62523739+xFatal1tYx@users.noreply.github.com>
2023-01-05 22:18:09 +0300

    Create 12.java

---
+++ 5 Ввод-вывод, доступ к файловой системе/5.1 Доступ к файловой системе./12.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String... args) throws IOException {
+        Path notEmptyDirectory = Path.of("C:\\123");
+
+        // Exception: DirectoryNotEmptyException
+        Files.deleteIfExists(notEmptyDirectory);
+
+        // Удаление с помощью перебора через стрим и метода Files.walk()
+        deleteByStream(notEmptyDirectory);
+
+        // Удаление с помощью класса SimpleFileVisitor и метода Files.walkFileTree()
+        deleteByWalkFileTree(notEmptyDirectory);
+    }
+
+    public static void deleteByWalkFileTree(Path notEmptyDirectory) throws IOException {
+        Files.walkFileTree(notEmptyDirectory, new SimpleFileVisitor<>() {
+                    // Вложенный класс SimpleFileVisitor
+
+                    // Удаление вложенных файлов
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    // Удаление родительской директории
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                }
+        );
+    }
+
+    static void deleteByStream(Path notEmptyDirectory) throws IOException {
+        if (!Files.exists(notEmptyDirectory)) {
+            return;
+        }
+
+        // Обратная сортировка позволяет сначала удалить все вложенные файлы
+        try (Stream<Path> files = Files.walk(notEmptyDirectory).sorted(Comparator.reverseOrder())) {
+            files.forEach(file -> {
+                try {
+                    Files.delete(file);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+}
